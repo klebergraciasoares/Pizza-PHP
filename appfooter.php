223535f84fb57484f3ffa565844d765d874a02b9
Robert Pamely <rpamely@googlemail.com>
2012-04-28 23:15:44 +0200

    Update appfooter.php

--- appfooter.php
+++ appfooter.php
@@ -1,3 +1,3 @@
 <?php
-// This is where you put your code for the end of the page when you're not using a deconstructor. e.g. closing a connection
+// This is where you put your code for the end of the page when you're not using a destructor. e.g. closing a connection
 ?>
